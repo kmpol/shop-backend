836f27575d682a67a30ef9ab4a9797abb2996624
Karol <malickikarol0@gmail.com>
2023-11-26 17:08:33 +0100

    Added product feature

---
+++ src/main/java/pl/karol/onlineshop/product/model/Product.java
@@ -0,0 +1,19 @@
+package pl.karol.onlineshop.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+
+    private String name;
+    private String category;
+    private String description;
+    private BigDecimal price;
+    private String currency;
+
+
+}
