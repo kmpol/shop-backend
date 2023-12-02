8c88f16d4fc58f107165ede9c27f02dac67f72cd
Karol <malickikarol0@gmail.com>
2023-12-02 12:26:11 +0100

    Added pagination for products, small refactor

--- src/main/java/pl/karol/onlineshop/product/controller/ProductController.java
+++ src/main/java/pl/karol/onlineshop/product/controller/ProductController.java
@@ -1,26 +1,24 @@
 package pl.karol.onlineshop.product.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.karol.onlineshop.product.model.Product;
 import pl.karol.onlineshop.product.service.ProductService;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @GetMapping("/api/v1/products")
-    public List<Product> getProducts() {
-        return productService.getProducts();
+    @GetMapping
+    public Page<Product> getProducts(Pageable pageable) {
+        return productService.getProducts(pageable);
     }
 
 }
--- src/main/java/pl/karol/onlineshop/product/service/ProductService.java
+++ src/main/java/pl/karol/onlineshop/product/service/ProductService.java
@@ -1,11 +1,11 @@
 package pl.karol.onlineshop.product.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pl.karol.onlineshop.product.model.Product;
 import pl.karol.onlineshop.product.repository.ProductRepository;
-
-import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,8 +17,8 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 
 }
